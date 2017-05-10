eb363d29225204324f4659cef47ccbd5071ab3e7
Nadia Cuadros <november138@hotmail.com>
2017-05-10 06:34:12 -0500

    mas estilo al nav

--- js/app.js
+++ js/app.js
@@ -1,3 +1,25 @@
-/*
- * Archivo principal de JS
- */
+var ultimoScrollTop = 0;
+window.addEventListener('scroll',function(){
+  var navegador = document.getElementById("navegador");
+	var accionScroll = document.documentElement.scrollTop;
+	if (accionScroll > ultimoScrollTop) {
+		navegador.classList.add('');
+
+			signup.style.transform = "translate(-200px, 0px)";
+		for (var i = 0; i < menuList.length; i++) {
+			menuList[i].classList.add('scroll-header-color');
+			logIn.classList.add('scroll-header-border');
+		}
+	}
+	else if (accionScroll <= 3) {
+		navHeader.classList.remove('bg-header');
+		logoHeader.setAttribute('src','assets/img/logo-white.png');
+		signup.style.transform = "translate(0px, 0px)";
+
+		for (var i = 0; i < menuList.length; i++) {
+			menuList[i].classList.remove('scroll-header-color');
+		}
+		logIn.classList.remove('scroll-header-border');
+	}
+	ultimoScrollTop = accionScroll;
+},false);
