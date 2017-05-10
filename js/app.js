52511e2eef8c1c186fe23c35c66f239b18c67a16
Nadia Cuadros <november138@hotmail.com>
2017-05-10 12:35:30 -0500

    intentando que fuencione bien el nav. Aplicando flex a las secciones: inicio y portafolio

--- js/app.js
+++ js/app.js
@@ -1,25 +1,15 @@
 var ultimoScrollTop = 0;
 window.addEventListener('scroll',function(){
   var navegador = document.getElementById("navegador");
-	var accionScroll = document.documentElement.scrollTop;
-	if (accionScroll > ultimoScrollTop) {
-		navegador.classList.add('nav-small');
+  if(document.body.scrollTop> 15 || document.documentElement.scrollTop > 15){
+    navegador.classList.add("nav-small");
+    console.log(scroll);
+  }
+  else{
+    navegador.classList.remove("nav-small");
+  }
 
-			signup.style.transform = "translate(-200px, 0px)";
-		for (var i = 0; i < menuList.length; i++) {
-			menuList[i].classList.add('scroll-header-color');
-			logIn.classList.add('scroll-header-border');
-		}
-	}
-	else if (accionScroll <= 3) {
-		navHeader.classList.remove('bg-header');
-		logoHeader.setAttribute('src','assets/img/logo-white.png');
-		signup.style.transform = "translate(0px, 0px)";
-
-		for (var i = 0; i < menuList.length; i++) {
-			menuList[i].classList.remove('scroll-header-color');
-		}
-		logIn.classList.remove('scroll-header-border');
-	}
-	ultimoScrollTop = accionScroll;
-},false);
+});
+var portafolio = document.getElementById("portafolio");
+var nosotros = document.getElementById("nosotros");
+var contacto = document.getElementById("contacto");
